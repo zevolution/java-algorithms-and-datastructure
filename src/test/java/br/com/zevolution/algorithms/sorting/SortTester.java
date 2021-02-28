be80d091aad43d9f188cbd0e4d0ff6c934f1f869
zevolution <jose122black@gmail.com>
2021-02-27 22:06:47 -0300

    Add method 'testSort()' in SortTester

--- src/test/java/br/com/zevolution/algorithms/sorting/SortTester.java
+++ src/test/java/br/com/zevolution/algorithms/sorting/SortTester.java
@@ -1,5 +1,15 @@
 package br.com.zevolution.algorithms.sorting;
 
+import static br.com.zevolution.algorithms.sorting.SortingTestData.*;
+import static org.junit.Assert.*;
+
 public class SortTester {
+
+	public static void testSort(Sort sorter) {
+		assertArrayEquals(SORTED_BY_LOWEST_PRICE, sorter.sort(SORTED_BY_HIGHEST_PRICE, SORTED_BY_HIGHEST_PRICE.length));
+		assertArrayEquals(SORTED_BY_LOWEST_PRICE, sorter.sort(SORTED_BY_LOWEST_PRICE, SORTED_BY_LOWEST_PRICE.length));
+		assertArrayEquals(SORTED_BY_LOWEST_PRICE, sorter.sort(NOT_SORTED, NOT_SORTED.length));
+		assertArrayEquals(SAME_PRODUCT, sorter.sort(SAME_PRODUCT, SAME_PRODUCT.length));
+	}
 
 }
