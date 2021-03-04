76308586b1884f2c139a0e697c2efd6a115a8e90
zevolution <jose122black@gmail.com>
2021-03-03 23:52:06 -0300

    Added test to prevents modification in original array

--- src/test/java/br/com/zevolution/algorithms/sorting/SortTester.java
+++ src/test/java/br/com/zevolution/algorithms/sorting/SortTester.java
@@ -3,6 +3,8 @@ package br.com.zevolution.algorithms.sorting;
 import static br.com.zevolution.algorithms.sorting.SortingTestData.*;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 public class SortTester {
 
 	public static void testSort(Sort sorter) {
@@ -10,6 +12,10 @@ public class SortTester {
 		assertArrayEquals(SORTED_BY_LOWEST_PRICE, sorter.sort(SORTED_BY_LOWEST_PRICE, SORTED_BY_LOWEST_PRICE.length));
 		assertArrayEquals(SORTED_BY_LOWEST_PRICE, sorter.sort(NOT_SORTED, NOT_SORTED.length));
 		assertArrayEquals(SAME_PRODUCT, sorter.sort(SAME_PRODUCT, SAME_PRODUCT.length));
+
+		// Make sure which original array it's not modified
+		sorter.sort(NOT_SORTED, NOT_SORTED.length);
+		assertFalse(Arrays.equals(SORTED_BY_LOWEST_PRICE, NOT_SORTED));
 	}
 
 }
